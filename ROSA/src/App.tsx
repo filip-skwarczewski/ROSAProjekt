3445ec3b3553cc64094e6f3a994e235a76b3b9d5
Filip Skwarczewski <filip.skwarczewski@gmail.com>
2026-06-07 22:08:41 +0200

    Started working, made the UI, but not finished, need to add the days and the clients bar

---
+++ ROSA/src/App.tsx
@@ -0,0 +1,22 @@
+
+
+function App() {
+  return (
+    <>
+    <div className="bg-blue-900 flex justify-center items-center rounded-2xl mt-1 w-fit mx-auto">
+      <h1 className="text-4xl font-bold text-blue-100 text-center p-4 ml-50 mr-50">ROSA</h1>
+    </div>
+    <div className="p-4">
+      <div className="bg-blue-300 p-4 rounded-3xl">
+          <h1 className="text-center font-bold text-2xl">Kalendarz</h1>
+          {/* <hr className="mt-3"/> */}
+          <div>
+            
+          </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
